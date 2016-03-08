ddaf3751a5166f104b8f2f21b4b8756b7a976fbf
skigth <fmjgomes@gmail.com>
2016-03-08 22:06:41 +0000

    Added the main variables and gets

--- src/main/java/pt/tecnico/mydrive/domain/User.java
+++ src/main/java/pt/tecnico/mydrive/domain/User.java
@@ -1,9 +1,29 @@
 package pt.tecnico.MyDrive.domain;
 
 public class User extends User_Base {
+    private String _username
     
+    private String _name;
+
+    private String _password;
+
+    private String _mask;
+
+
     public User() {
         super();
     }
-    
+    public String getUsername{
+      return _username;
+    }
+    public String getName{
+        return _name;
+    }
+    public String getPassword{
+        return _password;   
+    }
+    public String getMask{
+        return _mask;
+    }
+
 }
