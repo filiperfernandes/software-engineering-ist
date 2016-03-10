5a6cd38d6bb42f0376c737c8ca019ef26836268c
skigth <fmjgomes@gmail.com>
2016-03-10 16:37:56 +0000

    added some methods to SuperUser and User they have some errors and I'm not sure if I'm suposed to use exceptions at this point

--- src/main/java/pt/tecnico/mydrive/domain/SuperUser.java
+++ src/main/java/pt/tecnico/mydrive/domain/SuperUser.java
@@ -4,6 +4,23 @@ public class SuperUser extends SuperUser_Base {
     
     public SuperUser() {
         super();
+        setUsername("root");
+        setPassword("***");
+        setMask("rwxdr-x-");
     }
+
+    public String removeUser(String user){
+    	if (user.equals("root")){
+    		return "that user can't be removed";
+    		//Maybe throw and exception here
+    	}
+    	else{
+    		//remove the user object from the list
+    		//maybe remove his Directory?
+    		return "done";
+    	}
+    }
+
+
     
 }
