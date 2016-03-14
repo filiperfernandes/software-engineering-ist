9bede10fa8020a6fc87c79dfd8ab16779bc6c7dc
vultas <joao_vultos@hotmail.com>
2016-03-14 15:18:42 +0000

    Fixed menu added listUsers newUser and changeCurrentDirectory

--- src/main/java/pt/tecnico/MyDrive/MyDriveApplication.java
+++ src/main/java/pt/tecnico/MyDrive/MyDriveApplication.java
@@ -36,18 +36,19 @@ public class MyDriveApplication {
 
 	@Atomic
 	public static void display() {
+		PlainFile p = new PlainFile(null, null, null, null);
 		boolean  quit = true;
 		while(quit){
 			System.out.println("-- MENU --");
 			System.out.println(
 					"Select an option: \n" +
-							"  1) New File\n" +
+							"  1) List Users\n" +
 							"  2) New User\n" +
 							"  3) New Directory\n" +
 							"  4) New Plain File\n" +
-							"  5) List Directories\n " +
-							" 6) Remove Directory\n" +
-							"  7) Remove File\n" +
+							"  5) List Directories\n" +
+							"  6) Change Current Directory\n" +
+							"  7) Remove Directory\n" +
 							"  8) Remove Plain File\n" +
 							"  9) Show Plain File\n" +
 					"  0) EXIT\n");
@@ -57,10 +58,10 @@ public class MyDriveApplication {
 
 			switch (selection) {
 			case 1:
-				//newFile();
+				listUsers();
 				break;
 			case 2:
-				//newUser();
+				newUser();
 				break;
 			case 3:
 				//newDirectory();
@@ -69,26 +70,21 @@ public class MyDriveApplication {
 				//newPlainFile();
 				break;
 			case 5:
-				System.out.println("Insert path of directory");
-				listDirectory(input.next());
+				listDirectory();
 				break;
 			case 6:
-				//removeDirectory();
-				System.out.println("Insert name directory");
-				removeDirectory(input.next());
+				changeCurrentDirectory();
 				break;
 			case 7:
+				MyDrive md = MyDrive.getInstance();
+				System.out.println((md.getCurrentdir()).getName());
 				//removeFile();
-
 				break;
 			case 8:
 				//removePlainFile();
-				System.out.println("Insert name directory");
-				removePlainFile(input.next());
 				break;
 			case 9:
-				System.out.println("Insert path of file");
-				readPlainFile(input.next());
+				readPlainFile();
 				break;
 			case 0:
 				quit = false;
@@ -102,13 +98,63 @@ public class MyDriveApplication {
 			//			System.out.println("Exit Success!");
 		}
 	}
+	@Atomic
+	public static void changeCurrentDirectory(){
+		MyDrive md = MyDrive.getInstance();
 
-	/*private static void removeDirectory() {
-		// TODO Auto-generated method stub
-		}*/
+		System.out.println("Insert name of Directory you want to go");
+		String name = input.next();
+		if(name.equals(".")){
+			System.out.println("Still on same Directory");
+		}
+		else if(name.equals("..")){
+			md.setCurrentdir((md.getCurrentdir()).getDirectory());
+			System.out.println("Changed to previous Directory");
+		}
+		else {
+			for(File dir : (md.getCurrentdir()).getFileSet()){
+				if(name.equals(dir.getName())){
+					if(dir instanceof Directory ){
+						md.setCurrentdir((Directory) dir);
+						System.out.println("Changed current Directory to " + name);
+						break;
+					}
+					else{
+						// Exception
+						System.out.println("The File by that name is a PlainFile");
+					}
+				}
+			}
+			System.out.println("No File by that name in your current Directory");
+		}
+	}
+
+
 
 	@Atomic
-	public static void listDirectory(String path){
+	public static void newUser(){
+		System.out.println("Insert username of new User");
+		String username = input.next();
+		System.out.println("Insert name of new User");
+		String name = input.next();
+		System.out.println("Insert password of new User");
+		String pass = input.next();
+		User u = new User(username, pass, name);
+		System.out.println("ola");
+	}
+
+	@Atomic
+	public static void listUsers(){
+		MyDrive md = MyDrive.getInstance();
+		for(User u : md.getUserSet()){
+			System.out.println(u.getUsername());
+		}
+	}
+
+	@Atomic
+	public static void listDirectory(){
+		System.out.println("Insert path of directory");
+		String path = input.next();
 		MyDrive md = MyDrive.getInstance();
 		String dirname = "";
 		Directory dir = md.getRootdir() ;
@@ -135,7 +181,9 @@ public class MyDriveApplication {
 
 
 	@Atomic
-	public static void readPlainFile(String path) {
+	public static void readPlainFile() {
+		System.out.println("Insert path of file");
+		String path = input.next();
 		MyDrive md = MyDrive.getInstance();
 		String dirname = "";
 		Directory dir = md.getRootdir() ;
@@ -164,11 +212,10 @@ public class MyDriveApplication {
 		PlainFile file = new PlainFile(md.getCnt(), "test","rwxdr-test", "Hello World!");
 		Directory home = (Directory) (md.getRootdir()).getFileByName("home");
 		Directory root = (Directory) (home.getFileByName("root"));
-		Directory d = new Directory(md.getCnt(), "casa","rwxdr-test");
-
-		root.addFile(d);
 
 		root.addFile(file);
+
+		new User("joao", "passe", "vultos");
 
 		//new SuperUser();
 
@@ -178,9 +225,9 @@ public class MyDriveApplication {
 		//new User("Filipe", "test", "Filipe Fernandes");
 
 	}
+}
 
-
-	/*public static void createPlainFile(String owner, String name, String pathToFile, String permissions, String data){
+/*public static void createPlainFile(String owner, String name, String pathToFile, String permissions, String data){
 	@Atomic
 		MyDrive md = MyDrive.getInstance();
 		DateTime date = new DateTime();
@@ -192,7 +239,7 @@ public class MyDriveApplication {
 
 	}*/
 
-	/*	@Atomic
+/*	@Atomic
 	public static void test1() {
 
 		MyDrive md = MyDrive.getInstance();
@@ -213,37 +260,3 @@ public class MyDriveApplication {
 			e.printStackTrace();
 		}
 	}*/
-
-	@Atomic
-	public static void removeDirectory(String name ) {
-		MyDrive md = MyDrive.getInstance();
-		System.out.println("Name1: "+name);
-		if(((md.getCurrentdir()).equals(null))) 
-		{ 
-			//throws IOException 
-			System.out.println("Excepção Activa!");
-			System.out.println("Name2: "+name);
-
-		}else if ((md.getCurrentdir()).getFileByName(name).equals(null))
-		{
-			//throws IOException 
-			System.out.println("Excepção Activa!");
-			System.out.println("Name3: "+name);
-
-		}else { 
-			//Remove Directory
-			System.out.println("Name - Removido1: "+name);
-			(md.getCurrentdir()).removeFile((md.getCurrentdir()).getFileByName(name));
-			System.out.println("Name - Removido2: "+name);
-			//Continua nao remover
-
-		}}
-
-	@Atomic
-	public static void removePlainFile(String name){ //Remove PlainFile
-		MyDrive md = MyDrive.getInstance();
-		(md.getCurrentdir()).removeFile((md.getCurrentdir()).getFileByName(name));
-	}   //Continua nao remover
-}
-
-
--- src/main/java/pt/tecnico/MyDrive/domain/User.java
+++ src/main/java/pt/tecnico/MyDrive/domain/User.java
@@ -5,99 +5,85 @@ import org.joda.time.DateTime;
 
 
 public class User extends User_Base {
-	private String username;
-
+    
 	protected User(){}
-
-	public User(String username, String password, String name ) {
-		super();
+	
+    public User(String username, String password, String name ) {
+        super();
 		MyDrive md = MyDrive.getInstance();
 
-		if (checkValidString(username)==false || checkValidString(password)==false || checkValidString(name)==false){
-			return;
-			//Need tthrow an exception
-		}
-
 		setUsername(username);
+        setPassword(password);
+        setName(name);
+        setMask("rwxd----");
 
-		if (password == null){
-			setPassword(username);
-		}
-		else{
-			setPassword(password);
-		}
-
-		if(name==null){
-			setName(username);
-		}
-		else{
-			setName(name);
-		}   
-
-		setMask("rwxd----");
-
-		Directory dir = new Directory(md.getCnt(),username, "rwxdr-x-");
-		this.addFile(dir);
-
-		Directory home = (Directory) (md.getRootdir()).getFileByName("home");
+        Directory dir = new Directory(md.getCnt(),username, "rwxdr-x-");
+        this.addFile(dir);
+        
+        Directory home = (Directory) (md.getRootdir()).getFileByName("home");
 		home.addFile(dir);
+        md.addUser(this);
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public String ToString(){
+      return (getUsername() + " - " + getName() + " - " + getMask());
 
-	}
-
-	public String ToString(){
-		return (username + " - " + getName() + " - " + getMask());
-
-	}
-
-	public String changeUsername(String newUsername){
-
-		if (checkValidString(newUsername)==false){
-			return "Not a valid String";
-			//exception?
-		}
-		else if (newUsername.equals("root")){
-			return "error that name is reserved";
-			//throw exceptions?
-		}
-		else if (newUsername.equals(getName())){
-			return "your name was already that one";
-			//throw exceptions?
-		}
-		else{
-			setUsername(newUsername);
-			// setFilename(newUsername);
-			return  "sucess";
-		}
-
-		// falta verificar se o username pedido ja se encontra em uso+++
-	}
+    }
 
 
-	public String changeName(String newName){
-		if (checkValidString(newName)==false){
-			return "Not a valid String";
-			//exception?
-		}
-		else{
-			setName(newName);
-			return "sucess";
-		}
-	}
+    public String changeUsername(String newUsername){
+      
+      if (checkValidString(newUsername)==false){
+        return "Not a valid String";
+        //exception?
+      }
+      else if (newUsername.equals("root")){
+        return "error that name is reserved";
+        //throw exceptions?
+      }
+      else if (newUsername.equals(getName())){
+        return "your name was already that one";
+        //throw exceptions?
+      }
+      else{
+        setUsername(newUsername);
+       // setFilename(newUsername);
+        return  "sucess";
+      }
 
-	public boolean checkValidString(String check){
-		if (username==null){
-			return false;
-		}
-		else{}
-		for(int i=0; i<check.length();i++){
-			int asciiCheck = (int) check.charAt(i);
-			if (asciiCheck > 47 && asciiCheck < 58 || asciiCheck > 64 && asciiCheck <  91 || asciiCheck > 96 && asciiCheck < 123) {
-				continue;
-			}
-			return false;
-			//throw a exception  
-		}
-		return true;
-	}
+      // falta verificar se o username pedido ja se encontra em uso+++
+    }
+
+
+    public String changeName(String newName){
+      if (checkValidString(newName)==false){
+        return "Not a valid String";
+        //exception?
+      }
+      else{
+        setName(newName);
+        return "sucess";
+      }
+    }
+
+    public boolean checkValidString(String check){
+      for(int i=0; i<check.length();i++){
+          int asciiCheck = (int) check.charAt(i);
+        if (asciiCheck > 47 && asciiCheck < 58 || asciiCheck > 64 && asciiCheck <  91 || asciiCheck > 96 && asciiCheck < 123) {
+          continue;
+        }
+        return false;
+        //throw a exception  
+      }
+      return true;
+    }
 
 }
