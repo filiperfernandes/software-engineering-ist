6486ce39f4bd36a9dd8f20ed18022884307fcbc1
RicardoAlm <Rikardo.alm@hotmail.com>
2016-03-14 17:29:00 +0000

    Exc

--- src/main/java/pt/tecnico/MyDrive/MyDriveApplication.java
+++ src/main/java/pt/tecnico/MyDrive/MyDriveApplication.java
@@ -1,6 +1,7 @@
 package pt.tecnico.MyDrive;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import org.joda.time.DateTime;
 
@@ -106,14 +107,18 @@ public class MyDriveApplication {
 				c++;
 			}
 			else if(ch == '/'){
-				dir = (Directory) (dir.getFileByName(dirname));
-				dirname="";
+				try{
+					dir = (Directory) (dir.getFileByName(dirname));
+					dirname="";
+				}catch (FileDoesNotExistException e) { System.err.println(e); }
 			}
 			else{
 				dirname += ch;
 			}
 		}
-		dir = (Directory) (dir.getFileByName(dirname));
+		try{
+			dir = (Directory) (dir.getFileByName(dirname));
+		}catch (FileDoesNotExistException e) { System.err.println(e); }
 		System.out.println(".\n..");
 		for(File f : dir.getFileSet()){
 			System.out.println(f.getName());
