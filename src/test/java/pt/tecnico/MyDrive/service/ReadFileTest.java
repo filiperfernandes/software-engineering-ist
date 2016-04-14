2d532b4e4e2d53d04a8350ac9f2667ecb65c18dc
JBernardino <joao.bernardino@tecnico.ulisboa.pt>
2016-04-14 18:54:43 +0100

    Add ReadFileTest.java

---
+++ src/test/java/pt/tecnico/MyDrive/service/ReadFileTest.java
@@ -0,0 +1,20 @@
+package pt.tecnico.MyDrive.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReadFileTest extends AbstractServiceTest{
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Override
+	protected void populate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
