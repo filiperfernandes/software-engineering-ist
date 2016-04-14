4e01ba3279ba9f39f7737de8676070e0079ba425
JBernardino <joao.bernardino@tecnico.ulisboa.pt>
2016-04-14 18:32:29 +0100

    Add ChangeDirectory.java

---
+++ src/test/java/pt/tecnico/MyDrive/service/ChangeDirectoryTest.java
@@ -0,0 +1,20 @@
+package pt.tecnico.MyDrive.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChangeDirectoryTest extends AbstractServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Override
+	protected void populate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
