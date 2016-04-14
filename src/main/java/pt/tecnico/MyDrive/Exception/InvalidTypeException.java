1331337ebd87e0fa32d6d0a4bcacd3c169f82183
RicardoAlm <Rikardo.alm@hotmail.com>
2016-04-14 19:17:50 +0100

    add exception

---
+++ src/main/java/pt/tecnico/MyDrive/Exception/InvalidTypeException.java
@@ -0,0 +1,15 @@
+package pt.tecnico.MyDrive.Exception;
+
+public class InvalidTypeException extends MyDriveException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidTypeException() {
+	
+	
+		
+        super("Invalid Type\n");
+
+	}
+    
+}
