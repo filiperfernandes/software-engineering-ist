ed13984b52692cec894f8fecdaf60e879746fab7
skigth <fmjgomes@gmail.com>
2016-04-15 05:31:28 +0100

    Service ListDir

--- src/main/java/pt/tecnico/MyDrive/service/MyDriveService.java
+++ src/main/java/pt/tecnico/MyDrive/service/MyDriveService.java
@@ -3,7 +3,9 @@ package pt.tecnico.MyDrive.service;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import pt.tecnico.MyDrive.Exception.*;
 import pt.ist.fenixframework.Atomic;
+import pt.tecnico.MyDrive.domain.User;
 import pt.tecnico.MyDrive.domain.File;
 import pt.tecnico.MyDrive.domain.MyDrive;
 import pt.tecnico.MyDrive.domain.PlainFile;
@@ -35,5 +37,69 @@ import pt.tecnico.MyDrive.Exception.MyDriveException;
 
 	    protected abstract void dispatch() throws MyDriveException;
 	
+			public static boolean checkPermissionsRead(User user, User owner, String permissions){
+		char ch1[] = permissions.toCharArray();
+		String userPermissions = user.getMask();
+		char ch2[] = userPermissions.toCharArray();
+
+		if(owner.equals(user)){
+			return true;	
+		}
+		else if (ch1[5]==ch2[5] && ch1[5]=='r'){
+			return true;
+		}
+		else{
+			throw new UserDoesNotHavePermissionsException();
+		}
+
 	}
+
+	public static boolean checkPermissionsWrite(User user, User owner, String permissions){
+		char ch1[] = permissions.toCharArray();
+		String userPermissions = user.getMask();
+		char ch2[] = userPermissions.toCharArray();
+
+		if(owner.equals(user)){
+			return true;	
+		}
+		else if (ch1[6]==ch2[6] && ch1[6]=='w'){
+			return true;
+		}
+		else{
+			throw new UserDoesNotHavePermissionsException();
+		}
+	}
+
+	public static boolean checkPermissionsExecute(User user, User owner, String permissions){
+		char ch1[] = permissions.toCharArray();
+		String userPermissions = user.getMask();
+		char ch2[] = userPermissions.toCharArray();
+
+		if(owner.equals(user)){
+			return true;	
+		}
+		else if (ch1[7]==ch2[7] && ch1[7]=='x'){
+			return true;
+		}
+		else{
+			throw new UserDoesNotHavePermissionsException();
+		}
+	}
+	public static boolean checkPermissionsDelete(User user, User owner, String permissions){
+		char ch1[] = permissions.toCharArray();
+		String userPermissions = user.getMask();
+		char ch2[] = userPermissions.toCharArray();
+
+		if(owner.equals(user)){
+			return true;	
+		}
+		else if (ch1[8]==ch2[8] && ch1[8]=='d'){
+			return true;
+		}
+		else{
+			throw new UserDoesNotHavePermissionsException();
+		}
+	}
+	}
+
 
