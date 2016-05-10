7a81f8260bcd01116df83e604a4aefa3d3cbb77e
skigth <fmjgomes@gmail.com>
2016-05-10 22:09:22 +0100

    Execute presentation probably done

--- src/main/java/pt/tecnico/MyDrive/presentation/Execute.java
+++ src/main/java/pt/tecnico/MyDrive/presentation/Execute.java
@@ -1,9 +1,16 @@
 package pt.tecnico.MyDrive.presentation;
 import pt.tecnico.MyDrive.service.ExecuteFileService;
 
-public class Execute {
 
-	public Execute(){
+public class Execute extends MdCommand{
 
+	public Execute(Shell sh){
+		super(sh,"path","args to execute");
 	}
+	public void execute(String [] args){
+		if(args.length < 1)
+			throw new RuntimeException("USAGE: "+ name()+"");
+		new ExecuteFileService(args[0],args[1]).execute();
+	}
+
 }
