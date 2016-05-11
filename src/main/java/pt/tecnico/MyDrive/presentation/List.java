cfed79c0233d8c6da951c676504b3017bfff85b3
JBernardino <joao.bernardino@tecnico.ulisboa.pt>
2016-05-12 00:57:59 +0100

    closes #29 List

--- src/main/java/pt/tecnico/MyDrive/presentation/List.java
+++ src/main/java/pt/tecnico/MyDrive/presentation/List.java
@@ -1,19 +1,32 @@
 package pt.tecnico.MyDrive.presentation;
 
 import pt.tecnico.MyDrive.service.ListDirectoryService;
+import pt.tecnico.MyDrive.service.dto.FileInfoDto;
 
 public class List extends MdCommand{
+	
+	private MdShell s;
 
-	public List(Shell sh) { super(sh, "list", "'list <name>' to list a entries directory"); }
+	public List(Shell sh) { 
+		super(sh, "ls", "'list <name>' to list a entries directory"); 
+		s= ((MdShell) sh);
+	}
 
 	@Override
 	void execute(String[] args) {
 		if (args.length < 1){
-			throw new RuntimeException("USAGE: "+name()+" <ls> [path]");
+			ListDirectoryService l = new ListDirectoryService(s.getSessionToken(), "");
+			l.execute();
+			for(FileInfoDto f : l.result()){
+				System.out.println(f.getPermissions() + " " + f.getOwner() + " " + f.getId() + " " + f.getName());
+			}
 		}
-
-		if (args.length >1){
-			new ListDirectoryService(Long.valueOf(args[0])).execute();
+		else{
+			ListDirectoryService l = new ListDirectoryService(s.getSessionToken(), args[0]);
+			l.execute();
+			for(FileInfoDto f : l.result()){
+				System.out.println(f.getPermissions() + " " + f.getOwner() + " " + f.getId() + " " + f.getName());
+			}
 		}
 	}
 
