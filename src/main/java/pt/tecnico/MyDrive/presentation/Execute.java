f9d026a897b978d8a1bc31d2848cdf20fdd31c2e
skigth <fmjgomes@gmail.com>
2016-05-12 17:58:59 +0100

    Execute presentation minor tweaks

--- src/main/java/pt/tecnico/MyDrive/presentation/Execute.java
+++ src/main/java/pt/tecnico/MyDrive/presentation/Execute.java
@@ -4,11 +4,21 @@ import pt.tecnico.MyDrive.service.ExecuteFileService;
 
 public class Execute extends MdCommand{
 
-	public Execute(Shell sh){super(sh,"path","args to execute");}
-	public void execute(String [] args){
-		if(args.length < 1)
-			throw new RuntimeException("USAGE: "+ name()+"");
-		new ExecuteFileService(Integer.parseInt(args[0]),args[1],args[2]).execute();
+	private MdShell s;
+	public Execute(Shell sh){
+		super(sh,"path","args to execute");
+		s=((MdShell) sh);
 	}
+	public void execute(String [] args){
+		if(args.length < 1){
+			throw new RuntimeException("USAGE: "+ name()+"");
+		}
+		else{
+			ExecuteFileService e = new ExecuteFileService (s.getSessionToken(),args[0],args[1]);
+			e.execute();
+
+		}
+		System.out.println("got it");
+		}
 
 }
